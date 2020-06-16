31625e2da3d386c57f86bbce8848b8d8d4bed195
Ancient-Scapes <ryu.8224@gmail.com>
2020-06-16 18:18:56 +0900

    fix: <>

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ const routerBase =
   process.env.DEPLOY_ENV === 'GH_PAGES'
     ? {
       router: {
-        base: '/<nuxt-typescript-boilerplate>/'
+        base: '/nuxt-typescript-boilerplate/'
       }
     }
     : {}
