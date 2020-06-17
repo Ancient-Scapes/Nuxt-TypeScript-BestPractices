52afa7e1e65a91c4d5e811c44aba2933c93363ed
Ancient-Scapes <ryu.8224@gmail.com>
2020-06-18 06:38:22 +0900

    fix: overwrite vuetify a tag color

--- assets/pages/sidebar-menu-items.ts
+++ assets/pages/sidebar-menu-items.ts
@@ -2,6 +2,11 @@ import { ListItem } from '~/assets/interfaces/listItem'
 
 export const listItems: Array<ListItem> = [
   {
+    icon: 'mdi-home',
+    text: 'Top',
+    to: '/'
+  },
+  {
     icon: 'mdi-arrange-bring-forward',
     text: 'Modal',
     to: '/modal'
