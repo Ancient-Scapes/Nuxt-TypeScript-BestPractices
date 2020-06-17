313970dd81f623a772cf7404bfe80fde6b9c53f0
Ancient-Scapes <ryu.8224@gmail.com>
2020-06-18 06:22:00 +0900

    feat: add template vue file

---
+++ components/Template.vue
@@ -0,0 +1,25 @@
+<template>
+  <div></div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { ThisTypedComponentOptionsWithRecordProps } from 'vue/types/options'
+
+type Data = {}
+type Methods = {}
+type Computed = {}
+type Props = {}
+const options: ThisTypedComponentOptionsWithRecordProps<
+  Vue,
+  Data,
+  Methods,
+  Computed,
+  Props
+> = {
+  data () {
+    return {}
+  }
+}
+export default Vue.extend(options)
+</script>
