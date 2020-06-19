1a5dd172ba9dcf5cae7a0aa9077a2cd057c97134
Ancient-Scapes <ryu.8224@gmail.com>
2020-06-20 06:42:10 +0900

    おやすみ

--- pages/modal.vue
+++ pages/modal.vue
@@ -3,14 +3,6 @@
     <section>
       <h1>portal-vueを使ったモーダルサンプル</h1>
 
-      <h2>モーダルのレンダリング先(layouts/default.vue)</h2>
-      <!-- code -->
-
-      <h2>モーダルの定義(pages/modal.vue)</h2>
-      <!-- code -->
-
-      <h2>サンプル</h2>
-
       <v-btn @click="isMessageOpen = true">モーダルを開く</v-btn>
 
       <portal v-if="isMessageOpen" to="message">
@@ -33,6 +25,21 @@
         </div>
       </portal>
 
+      <h2>コードのリンク</h2>
+
+      <a href="https://github.com/Ancient-Scapes/Nuxt-TypeScript-Boilerplate/blob/5ba557c2ba21828de5ba1485096f5c8ae2b70aa4/layouts/default.vue#L12-L13">
+        <div class="heading-link">
+          <span>モーダルのレンダリング先(layouts/default.vue)</span>
+          <v-icon>mdi-arrange-bring-forward</v-icon>
+        </div>
+      </a>
+
+      <a href="https://github.com/Ancient-Scapes/Nuxt-TypeScript-Boilerplate/blob/5ba557c2ba21828de5ba1485096f5c8ae2b70aa4/pages/modal.vue#L16-L34">
+        <div class="heading-link">
+          <span>モーダルの定義(pages/modal.vue)</span>
+          <v-icon>mdi-arrange-bring-forward</v-icon>
+        </div>
+      </a>
     </section>
 
   </div>
@@ -65,6 +72,15 @@ export default Vue.extend(options)
 </script>
 
 <style lang="scss" scoped>
+.heading-link {
+  display: flex;
+  margin-bottom: 22px;
+
+  i {
+    margin-left: 10px;
+  }
+}
+
 .modal {
   // Vuetify z-index vs Modal z-index
   z-index: 999999;
