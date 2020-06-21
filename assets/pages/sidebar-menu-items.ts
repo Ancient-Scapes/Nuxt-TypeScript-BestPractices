42b8dda222b60b22a07d96345a88e19b8de88152
Ancient-Scapes <ryu.8224@gmail.com>
2020-06-22 05:37:41 +0900

    feat: add loading sample fix #5

--- assets/pages/sidebar-menu-items.ts
+++ assets/pages/sidebar-menu-items.ts
@@ -5,5 +5,11 @@ export const listItems: Array<ListItem> = [
     icon: 'mdi-arrange-bring-forward',
     text: 'Modal',
     to: '/modal'
+  },
+  {
+    // loading iconはなんか違う
+    icon: 'mdi-reload',
+    text: 'Loading',
+    to: '/loading'
   }
 ]
---
+++ pages/loading.vue
@@ -0,0 +1,63 @@
+<template>
+  <section>
+    <h1>vue-spinnerを使ったLoadingサンプル</h1>
+
+    <v-btn v-if="!isLoadingComplete" @click="isLoadingComplete = true">ロード終了</v-btn>
+    <v-btn v-else @click="isLoadingComplete = false">ロード開始</v-btn>
+
+    <div class="contents">
+      <OrbitSpinner
+        v-if="!isLoadingComplete"
+        :animation-duration="1200"
+        :size="55"
+        color="gray"
+      />
+
+      <div v-else class="contents__load-complete">
+        <p>Loading complete...display something contents here</p>
+      </div>
+
+    </div>
+
+  </section>
+
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { ThisTypedComponentOptionsWithRecordProps } from 'vue/types/options'
+import { OrbitSpinner } from 'epic-spinners'
+
+type Data = {
+  isLoadingComplete: boolean
+}
+type Methods = {}
+type Computed = {}
+type Props = {}
+const options: ThisTypedComponentOptionsWithRecordProps<
+  Vue,
+  Data,
+  Methods,
+  Computed,
+  Props
+> = {
+  components: {
+    OrbitSpinner
+  },
+  data () {
+    return {
+      isLoadingComplete: false
+    }
+  }
+}
+export default Vue.extend(options)
+</script>
+
+<style lang="scss" scoped>
+.contents {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 30px 0 15px 0;
+}
+</style>
